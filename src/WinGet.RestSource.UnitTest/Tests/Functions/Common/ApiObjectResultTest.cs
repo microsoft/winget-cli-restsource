86ea21482d48a83542b91efb7ad66f1a968159bc
Zach Carpenter <carpenter.zach@gmail.com>
2021-03-31 20:53:24 -0700

    Add Initial Test Project & Build (#19)
    This is to add an initial test project as well as onboard a build.

---
+++ src/WinGet.RestSource.UnitTest/Tests/Functions/Common/ApiObjectResultTest.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ApiObjectResultTest.cs" company="Microsoft Corporation">
+//     Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Microsoft.Winget.RestSource.UnitTest.Tests.Functions.Common
+{
+    using Microsoft.WinGet.RestSource.Functions.Common;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// API Object Result Test.
+    /// </summary>
+    public class ApiObjectResultTest
+    {
+        private ITestOutputHelper log;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApiObjectResultTest"/> class.
+        /// </summary>
+        /// <param name="log">ITestOutputHelper.</param>
+        public ApiObjectResultTest(ITestOutputHelper log)
+        {
+            this.log = log;
+        }
+
+        /// <summary>
+        /// This verifies that the default object results are set as expected.
+        /// </summary>
+        [Fact]
+        public void Default()
+        {
+            string objectResult = "objectResult";
+            ApiObjectResult apiObjectResult = new ApiObjectResult(objectResult);
+            Assert.Equal(200, apiObjectResult.StatusCode);
+        }
+    }
+}
---
+++ src/WinGet.RestSource.UnitTest/Tests/RestSource/Common/FormatJSONTest.cs
@@ -0,0 +1,51 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="FormatJSONTest.cs" company="Microsoft Corporation">
+//     Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Microsoft.Winget.RestSource.UnitTest.Tests.RestSource.Common
+{
+    using System.Collections.Generic;
+    using Microsoft.WinGet.RestSource.Common;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// Format JSON Tests.
+    /// </summary>
+    public class FormatJSONTest
+    {
+        private ITestOutputHelper log;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FormatJSONTest"/> class.
+        /// </summary>
+        /// <param name="log">ITestOutputHelper.</param>
+        public FormatJSONTest(ITestOutputHelper log)
+        {
+            this.log = log;
+        }
+
+        /// <summary>
+        /// This verifies that JSON object is formatted and not indented.
+        /// </summary>
+        [Fact]
+        public void None()
+        {
+            List<string> objectToFormat = new List<string>
+            {
+                "a",
+                "b",
+                "c",
+            };
+
+            FormatJSON.None(objectToFormat);
+            this.log.WriteLine($"Object to Format: {FormatJSON.Indented(objectToFormat)}");
+
+            string expected = @"[""a"",""b"",""c""]";
+            string formattedJson = FormatJSON.None(objectToFormat);
+            Assert.Equal(expected, formattedJson);
+        }
+    }
+}
