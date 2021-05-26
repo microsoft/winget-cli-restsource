637d88941a9e35eb908804b72393ef93304eaeb0
Ashwini Patil <ashpatil@microsoft.com>
2021-05-26 16:13:23 -0700

    Update license information

--- src/WinGet.RestSource/Cosmos/ICosmosDatabase.cs
+++ src/WinGet.RestSource/Cosmos/ICosmosDatabase.cs
@@ -1,6 +1,6 @@
 ﻿// -----------------------------------------------------------------------
 // <copyright file="ICosmosDatabase.cs" company="Microsoft Corporation">
-//     Copyright (c) Microsoft Corporation. All rights reserved.
+//     Copyright (c) Microsoft Corporation. Licensed under the MIT License.
 // </copyright>
 // -----------------------------------------------------------------------
 
