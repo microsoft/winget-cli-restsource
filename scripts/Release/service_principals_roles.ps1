240d018255dc1852076298745e087e3600ccd372
Ruben Guerrero Samaniego <rubengu@microsoft.com>
2024-06-18 16:03:59 -0700

    Script

--- scripts/Release/service_principals_roles.ps1
+++ scripts/Release/service_principals_roles.ps1
@@ -1,8 +1,8 @@
 <#
 .SYNOPSIS
-    Sets roles to a service principals
+    Sets roles to a different azure resources.
 .DESCRIPTION
-    Sets roles to service principals given a json file with the definitions.
+    Sets the roles for different service principal for different resources.
 #>
 Param(
     [Parameter(Mandatory=$true)]
@@ -21,16 +21,50 @@ Param(
     $showWarnings
 )
 
-Set-AzContext -SubscriptionId $subscriptionId | Out-Null
-$local:data = Get-Content $rolesFile -Raw | ConvertFrom-Json
-
-foreach ($storageAccount in $data.storageAccounts)
+enum ResourceType
 {
-    $private:scope = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Storage/storageAccounts/$($storageAccount.name)"
+    StorageAccount
+    AppConfig
+}
 
-    foreach ($sp in $storageAccount.servicePrincipals)
+function GetScope {
+    param (
+        [Parameter(Mandatory=$true)]
+        [ResourceType]
+        $resourceType,
+        [Parameter(Mandatory=$true)]
+        [String]
+        $resourceName
+    )
+
+    if ($resourceType -eq [ResourceType]::StorageAccount)
     {
-        Write-Host "Configuring $($sp.displayName) in $($storageAccount.name)"
+        return "/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Storage/storageAccounts/$resourceName"
+    }
+    elseif ($resourceType -eq [ResourceType]::AppConfig)
+    {
+        return "/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.AppConfiguration/configurationStores/$resourceName"
+    }
+    else
+    {
+        throw "Not supported"
+    }
+}
+
+function AssignRoles {
+    param (
+        [Parameter(Mandatory=$true)]
+        [ResourceType]
+        $resourceType,
+        [Parameter(Mandatory=$true)]
+        [PSCustomObject]$resource
+    )
+
+    $private:scope = GetScope $resourceType $($resource.name)
+
+    foreach ($sp in $resource.servicePrincipals)
+    {
+        Write-Host "Configuring $($sp.displayName) in $($resource.name)"
         $private:objectId = (Get-AzADServicePrincipal -DisplayName $($sp.displayName)).Id
 
         if ($null -eq $objectId)
@@ -51,9 +85,22 @@ foreach ($storageAccount in $data.storageAccounts)
             {
                 if ($showWarnings)
                 {
-                    Write-Warning "$($sp.displayName) already has '$role' assigned to $($storageAccount.name)"
+                    Write-Warning "$($sp.displayName) already has '$role' assigned to $($resource.name)"
                 }
             }
         }
     }
+}
+
+Set-AzContext -SubscriptionId $subscriptionId | Out-Null
+$local:data = Get-Content $rolesFile -Raw | ConvertFrom-Json
+
+foreach ($storageAccount in $data.storageAccounts)
+{
+    AssignRoles StorageAccount $storageAccount
+}
+
+foreach ($appConfig in $data.appConfigs)
+{
+    AssignRoles AppConfig $appConfig
 }
